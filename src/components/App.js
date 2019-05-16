12e41da83daf3d2c73da2d56709c4ae1129af445
angel <angelsol299@hotmail.com>
2019-05-16 11:54:40 +0200

    configuring axios

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit = term => {
+    axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization:
+          "Client-ID 62ac3ef66258fac175e0a0dd288103288b33d69d1ba6664e42d50df634c03c0b"
+      }
+    });
     console.log(term);
   };
 
